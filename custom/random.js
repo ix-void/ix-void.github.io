aaba2dc830ed9042da9893279eba9187aaa60456
time-tso <timepython@outlook.com>
2024-09-04 20:32:32 +0800

    Site updated: 2024-09-04 20:32:32

--- custom/random.js
+++ custom/random.js
@@ -1,1 +1,1 @@
-var posts=["2024-08/hello-world/","2024-08/how-to-progress/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2024-08/lookup-lookfor-find-different/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
