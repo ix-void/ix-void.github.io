726934fe8ea77d287e491d0f16721b8c83b46a55
time-tso <timepython@outlook.com>
2024-12-12 22:36:00 +0800

    Site updated: 2024-12-12 22:36:00

--- custom/random.js
+++ custom/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/2024/12/Butterfly-Square-Avatar/","posts/2024/09/Entrance-Examination-Chinese-Must-Memorize-The-Articles/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/2024/09/Entrance-Examination-Chinese-Must-Memorize-The-Articles/","posts/2024/12/Butterfly-Square-Avatar/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
